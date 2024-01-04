6fe9784fc635cb8f3f940c27c230cbbde3caa8f5
rishi1103 <56641335+rishi1103@users.noreply.github.com>
2024-01-04 23:24:50 +0530

    Create Oset_syntax.cpp

---
+++ Data structures/Oset_syntax.cpp
@@ -0,0 +1,7 @@
+#include <ext/pb_ds/assoc_container.hpp> 
+#include <ext/pb_ds/tree_policy.hpp> 
+using namespace __gnu_pbds; 
+  
+#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update> 
+
+// functions : insert(), erase(), find_by_order(), order_of_key(), find()
